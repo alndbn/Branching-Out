c58cc54b38c879dc7105a4ce1ee9a00dc05800d0
Alena Drobner <alena.drobner@gmail.com>
2025-10-17 14:22:06 +0200

    Update input prompt to include email option

--- filter_users.py
+++ filter_users.py
@@ -33,7 +33,7 @@ def filter_by_email(mail):
         print(user)
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Name or Age): ").strip().lower()
+    filter_option = input("What would you like to filter by? (Name, Age or Email): ").strip().lower()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
